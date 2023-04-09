fb2e2ad5a62d0b96ac817e657e66fdc11b0e31da
YoungPentagonHacker <84976745+YoungPentagonHacker@users.noreply.github.com>
2023-04-09 17:45:58 +0300

    Update Caesar.h

--- cryptoalgorithms/Caesar.h
+++ cryptoalgorithms/Caesar.h
@@ -6,7 +6,6 @@
 class Caesar : IEncryptor{
 public:
     std::string encrypt(std::string key, std::string msg) override;
-    std::ofstream encryptFile(std::string key, std::ifstream msg) override;
     
     std::string decrypt(std::string key, std::string msg)  override;    
 };
