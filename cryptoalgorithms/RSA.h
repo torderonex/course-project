669784210c4aee9d051c9faf327969e6485c8991
YoungPentagonHacker <eligorko.info@yandex.ru>
2023-04-09 17:44:24 +0300

    caesar cipher added + rsa in development

--- cryptoalgorithms/RSA.h
+++ cryptoalgorithms/RSA.h
@@ -3,9 +3,9 @@
 #include "IEncryptor.h"
 #include <fstream>
 
-class RSA {
+class RSA : IEncryptor{
 public:
-    std::string encrypt(std::string key, std::string msg); //override;
+    std::string encrypt(std::string key, std::string msg) override;
     
-   std::string decrypt(std::string key, std::string msg) ;  //override;    
+   std::string decrypt(std::string key, std::string msg) override;    
 };
